26ad5a1fee312309e75020a480cf4ea4da57c361
muhamedhamedvl <mh1191128@gmail.com>
2025-08-24 12:14:48 +0300

    UserRole.cs

---
+++ CourseManagement.DAL/Enums/UserRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagement.DAL.Enums
+{
+    public enum UserRole
+    {
+        Admin,
+        Instructor,
+        Student
+    }
+}
