b8d028f00e2fbf9343dd7d715049a36a4da5e80a
muhamedhamedvl <mh1191128@gmail.com>
2025-08-24 13:43:08 +0300

    Program.cs => DI

--- CourseManagement.Web/Program.cs
+++ CourseManagement.Web/Program.cs
@@ -1,3 +1,10 @@
+using CourseManagement.BLL.Interfaces;
+using CourseManagement.BLL.Services;
+using CourseManagement.DAL.Data;
+using CourseManagement.DAL.Interfaces;
+using CourseManagement.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 namespace CourseManagement.Web
 {
     public class Program
@@ -6,19 +13,23 @@ namespace CourseManagement.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
 
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            //DI
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IUserService, UserService>();
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseRouting();
 
