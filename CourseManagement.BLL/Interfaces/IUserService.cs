b7b70bcaaa1ad9a10b6b965be8399b9443c1db67
muhamedhamedvl <mh1191128@gmail.com>
2025-08-24 12:13:31 +0300

    UserService , IUserService Implemented

---
+++ CourseManagement.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using CourseManagement.DAL.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagement.BLL.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetAllAsync(string search, string role, int page, int pageSize);
+        Task<User> GetByIdAsync(int id);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(int id);
+        Task<bool> EmailExistsAsync(string email, int? excludeUserId = null);
+    }
+}
---
+++ CourseManagement.BLL/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using CourseManagement.BLL.Interfaces;
+using CourseManagement.DAL.Interfaces;
+using CourseManagement.DAL.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagement.BLL.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public Task<IEnumerable<User>> GetAllAsync(string search, string role, int page, int pageSize) =>
+            _userRepository.GetAllAsync(search, role, page, pageSize);
+
+        public Task<User> GetByIdAsync(int id) =>
+            _userRepository.GetByIdAsync(id);
+
+        public Task AddAsync(User user) =>
+            _userRepository.AddAsync(user);
+
+        public Task UpdateAsync(User user) =>
+            _userRepository.UpdateAsync(user);
+
+        public Task DeleteAsync(int id) =>
+            _userRepository.DeleteAsync(id);
+
+        public Task<bool> EmailExistsAsync(string email, int? excludeUserId = null) =>
+            _userRepository.EmailExistsAsync(email, excludeUserId);
+    }
+}
