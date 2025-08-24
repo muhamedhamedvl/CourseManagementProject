93fc5613967981fb5f60e2a863995422fe9dbffe
muhamedhamedvl <mh1191128@gmail.com>
2025-08-24 12:18:06 +0300

    User.cs

---
+++ CourseManagement.DAL/Models/Users/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using CourseManagement.DAL.Enums;
+
+namespace CourseManagement.DAL.Models.Users
+{
+   
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(50, MinimumLength = 3)]
+        public string? Name { get; set; }
+
+        [Required, EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public UserRole? Role { get; set; } 
+    }
+}
