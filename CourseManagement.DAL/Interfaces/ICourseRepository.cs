46bff18d7db9e8110b36e297e280fc2dcd5c5c31
FatmaMohammed-ui <fatmamohamed12341234z@gmail.com>
2025-08-26 15:30:49 +0300

    Add files via upload

---
+++ CourseManagement.DAL/Interfaces/ICourseRepository.cs
@@ -0,0 +1,19 @@
+﻿
+using DAL.Entitys;
+
+
+namespace DAL.Interfaces
+{
+    public interface ICourseRepository
+    {
+        Task<IEnumerable<Course>> GetAllAsync();
+        Task<Course?> GetByIdAsync(int id);
+        Task AddAsync(Course entity);
+        void Update(Course entity);
+        void Remove(Course entity);
+        Task<bool> ExistsByNameAsync(string name, int? excludeId = null);
+        Task<int> SaveAsync(); // Save() for Course
+        int GetCount(string? searchNane = null);
+        IEnumerable<Course> GetPage(int page, int pageSize, string? SearchName = null);
+    }
+}
