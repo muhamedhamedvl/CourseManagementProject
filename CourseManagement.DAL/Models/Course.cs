f9a2d5963a503f37f7e3f4759c73171c697bc099
FatmaMohammed-ui <fatmamohamed12341234z@gmail.com>
2025-08-26 15:32:54 +0300

    Add files via upload

---
+++ CourseManagement.DAL/Models/Course.cs
@@ -0,0 +1,53 @@
+﻿using DAL.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace DAL.Entitys
+{
+    public class Course
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [Remote(action: "IsCourseNameUnique", controller: "Course", AdditionalFields = "Id")]
+        //[NoNumber]
+        public string Name { get; set; }
+        [Required]
+
+        public string Description { get; set; }
+        [Required]
+
+        public CourseCategory Category { get; set; }
+        [Required]
+
+        public DateTime StartDate { get; set; }
+        [Required]
+
+        public DateTime EndDate { get; set; }
+        [Required]
+
+        public bool IsActive { get; set; }
+        [Required]
+
+        public int InstructorId { get; internal set; }
+        public Instructor? Instructor { get; set; }
+        public class NoNumberAttribute : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                if (value != null && value.ToString().Any(char.IsDigit))
+                {
+                    return new ValidationResult("Course name cannot contain numbers.");
+                }
+                return ValidationResult.Success;
+            }
+        }
+    }
+}
+    
+
---
+++ CourseManagement.DAL/Models/Instructor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entitys
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Bio { get; set; }
+        public bool IsActive { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+}
