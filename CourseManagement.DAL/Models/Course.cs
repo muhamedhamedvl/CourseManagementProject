da1b8efeeb4d0068a86a1d75544c52d9b6613b72
FatmaMohammed-ui <fatmamohamed12341234z@gmail.com>
2025-08-26 15:35:20 +0300

    Update Course.cs

--- CourseManagement.DAL/Models/Course.cs
+++ CourseManagement.DAL/Models/Course.cs
@@ -1,53 +1,29 @@
-﻿using DAL.Enums;
-using System;
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Mvc;
+using System;
 
-namespace DAL.Entitys
+namespace CourseManagement.DAL.Models
 {
     public class Course
     {
-        [Required]
         public int Id { get; set; }
+
         [Required]
-        [Remote(action: "IsCourseNameUnique", controller: "Course", AdditionalFields = "Id")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
         //[NoNumber]
         public string Name { get; set; }
-        [Required]
 
-        public string Description { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Category is required")]
+        public string Category { get; set; }
 
-        public CourseCategory Category { get; set; }
-        [Required]
+        public string? Description { get; set; }
 
-        public DateTime StartDate { get; set; }
+        // FK
         [Required]
+        public int InstructorId { get; set; }
 
-        public DateTime EndDate { get; set; }
-        [Required]
-
-        public bool IsActive { get; set; }
-        [Required]
-
-        public int InstructorId { get; internal set; }
-        public Instructor? Instructor { get; set; }
-        public class NoNumberAttribute : ValidationAttribute
-        {
-            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-            {
-                if (value != null && value.ToString().Any(char.IsDigit))
-                {
-                    return new ValidationResult("Course name cannot contain numbers.");
-                }
-                return ValidationResult.Success;
-            }
-        }
+        [ForeignKey("InstructorId")]
+        public Instructor Instructor { get; set; }
+    
     }
 }
-    
-
